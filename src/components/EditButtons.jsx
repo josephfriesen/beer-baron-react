e5923a2f79a7f89290f30ea387880ee69979c4e7
Joseph Friesen <friesen.josephc@gmail.com>
2018-11-16 15:20:57 -0800

    Add popup dialog box to confirm keg deletion.

--- src/components/EditButtons.jsx
+++ src/components/EditButtons.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-toolbox/lib/button';
+import { Dialog } from 'react-toolbox/lib/dialog';
 
-export default function EditButtons(props) {
-  return (
-    <div className='container'>
-      <Button label='Sell a pint' raised onClick={props.sellAPint} />
-      <Button label='Restock keg' raised onClick={props.restock} />
-      <Button label='Delete this keg' raised onClick={props.removeKeg} />
-    </div>
-  );
+export default class EditButtons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deleteDialogOn: false
+    }
+    this.deleteDialogToggle = this.deleteDialogToggle.bind(this);
+    this.onDeleteConfirm = this.onDeleteConfirm.bind(this);
+    this.deleteDialogActions = [
+      { label: 'Cancel', onClick: this.deleteDialogToggle },
+      { label: 'Confirm', onClick: this.onDeleteConfirm }
+    ];
+  }
+  
+  deleteDialogToggle() {
+    this.setState({deleteDialogOn: !this.state.deleteDialogOn});
+  }
+  
+  onDeleteConfirm() {
+    this.deleteDialogToggle();
+    this.props.removeKeg();
+  }
+  
+  render() {
+    return (
+      <div className='container'>
+        <Button label='Sell a pint' raised onClick={this.props.sellAPint} />
+        <Button label='Restock keg' raised onClick={this.props.restock} />
+        <Button label='Delete this keg' raised onClick={this.deleteDialogToggle} />
+        <Dialog
+          actions={this.deleteDialogActions}
+          active={this.state.deleteDialogOn}
+          onEscKeyDown={this.deleteDialogToggle}
+          onOverlayClick={this.deleteDialogToggle}
+          title='Confirm'>
+          <div>Are you sure you want to delete this keg from inventory? This action cannot be undone.</div>
+        </Dialog>
+      </div>
+    );
+  }
 }
 
 EditButtons.propTypes = {
