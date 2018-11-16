cb2b56161b115f343b70698de1d212ac16b91cdb
Joseph Friesen <friesen.josephc@gmail.com>
2018-11-16 11:37:36 -0800

    Add popup dialog to employeeview component.

--- src/components/EmployeeView.jsx
+++ src/components/EmployeeView.jsx
@@ -2,20 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TapList from './TapList';
 import { Button } from 'react-toolbox/lib/button';
+import { Dialog } from 'react-toolbox/lib/dialog';
 
-function EmployeeView(props) {
-  return (
-    <div>
+
+export default class EmployeeView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addDialogOn: false
+    };
+    this.addDialogToggle = this.addDialogToggle.bind(this);
+    this.addDialogActions = [
+      { label: 'Cancel', onClick: this.addDialogToggle }
+    ];
+  };
+  
+  addDialogToggle() {
+    this.setState({addDialogOn: !this.state.addDialogOn});
+  };
+  
+  render() {
+    return (
       <div>
-        <TapList
-          kegs={props.kegs}
-          onActiveKegChange={props.onActiveKegChange} />
+        <div>
+          <TapList
+            kegs={this.props.kegs}
+            onActiveKegChange={this.props.onActiveKegChange} />
+        </div>
+        <div>
+          <Button label='Add a keg' raised onClick={this.addDialogToggle} />
+          <Dialog
+            actions={this.addDialogActions}
+            active={this.state.addDialogOn}
+            onEscKeyDown={this.addDialogToggle}
+            onOverlayClick={this.addDialogToggle}
+            title='Add a new keg'>
+            <p>What up</p>
+          </Dialog>
+        </div>
       </div>
-      <div>
-        <Button label='Add a keg' icon='add' />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 EmployeeView.propTypes = {
@@ -23,5 +50,3 @@ EmployeeView.propTypes = {
   routerPath: PropTypes.string,
   onActiveKegChange: PropTypes.func
 };
-
-export default EmployeeView;
