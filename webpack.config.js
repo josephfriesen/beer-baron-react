190eef6387f2af0a8e8b47094e4172859aaaab96
Joe Friesen <friesen.josephc@gmail.com>
2018-11-16 09:54:58 -0800

    Install and configure react-toolbox and postcss-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -82,6 +82,22 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader"
+        ]
       }
     ]
   },
